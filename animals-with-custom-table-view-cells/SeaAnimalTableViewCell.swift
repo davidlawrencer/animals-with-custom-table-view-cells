adc34f694944eb8f9286d686b3b994076f2d0c2b
davidlawrencer <davidlrifkin+github@gmail.com>
2019-08-12 14:59:21 -0400

    Adds table view cell for sea animals

---
+++ animals-with-custom-table-view-cells/SeaAnimalTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  SeaAnimalTableViewCell.swift
+//  animals-with-custom-table-view-cells
+//
+//  Created by David Rifkin on 8/12/19.
+//  Copyright © 2019 David Rifkin. All rights reserved.
+//
+
+import UIKit
+
+class SeaAnimalTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var seaAnimalImage: UIImageView!
+    
+    @IBOutlet weak var seaAnimalLabel: UILabel!
+    
+    @IBOutlet weak var edibleAnimalLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
